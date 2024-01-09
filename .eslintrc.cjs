3805f3b262f15eb79b497ec29aeb01f744879844
Abdullah149081 <masudmd425@gmail.com>
2024-01-09 21:10:22 +0600

    Project Setup

---
+++ .eslintrc.cjs
@@ -0,0 +1,32 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: [
+    'airbnb',
+    'airbnb/hooks',
+    'airbnb-typescript',
+    'eslint:recommended',
+    'plugin:react/recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:prettier/recommended',
+  ],
+  overrides: [],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+    project: './tsconfig.json',
+  },
+  plugins: ['@typescript-eslint', 'react', 'jsx-a11y', 'prettier'],
+  rules: {
+    'react/jsx-filename-extension': ['error', { extensions: ['.tsx'] }],
+    'react/react-in-jsx-scope': 0,
+    'react/prop-types': ['off', {}],
+    'no-use-before-define': 'off',
+    '@typescript-eslint/no-use-before-define': ['error'],
+    'no-shadow': 'off',
+    '@typescript-eslint/no-shadow': 'error',
+  },
+};
