3fe5dc06283f4579bdab04dce41edabb17252955
Abdullah149081 <masudmd425@gmail.com>
2024-01-10 23:10:42 +0600

    form component add

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import './App.css';
 
 import UseState from './Hooks/UseState';
+import Form from './component/Form';
 
 function App() {
   return (
     <div>
       <UseState />
+      <Form />
     </div>
   );
 }
