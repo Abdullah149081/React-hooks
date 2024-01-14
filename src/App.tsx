d6ff6573bb0017dab2df1e22fb0591ad86314f3f
Abdullah149081 <masudmd425@gmail.com>
2024-01-14 20:23:52 +0600

    done

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,29 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
 import './App.css';
-
+import UseEffect from './Hooks/UseEffect';
+import UseReducer from './Hooks/UseReducer';
+import UseRef from './Hooks/UseRef';
 import UseState from './Hooks/UseState';
+import useDark from './Hooks/useDark';
+import DarkTheme from './component/DarkTheme';
 import Form from './component/Form';
+import FormReducer from './component/FormReducer';
 
 function App() {
+  const [dark] = useDark();
   return (
-    <div>
-      <UseState />
-      <Form />
+    <div
+      className={` flex h-screen items-center justify-center ${
+        dark ? 'bg-slate-900' : 'bg-white'
+      }`}
+    >
+      {/* <UseState /> */}
+      {/* <UseReducer /> */}
+      {/* <Form /> */}
+      {/* <FormReducer /> */}
+      {/* <UseEffect /> */}
+      {/* <UseRef /> */}
+      <DarkTheme />
     </div>
   );
 }
